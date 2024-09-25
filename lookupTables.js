aee4190a593fa05b6a1f06f40d43b54b47337c86
thanh1412-del <kaitokidto08141@gmail.com>
2024-09-25 20:47:52 +0700

    Add files via upload

---
+++ lookupTables.js
@@ -0,0 +1,14 @@
+// lookupTables.js
+export const absoluteHumidityTable = [
+    { temp: 10, humidity: 30, absoluteHumidity: 4.5 },
+    { temp: 10, humidity: 40, absoluteHumidity: 5.6 },
+    { temp: 20, humidity: 30, absoluteHumidity: 7.2 },
+    // Thêm các giá trị khác
+];
+
+export const dewPointTableAbsoluteHumidity = [
+    { absoluteHumidity: 4.5, dewPoint: 0 },
+    { absoluteHumidity: 5.6, dewPoint: 2 },
+    { absoluteHumidity: 7.2, dewPoint: 5 },
+    // Thêm các giá trị khác
+];
